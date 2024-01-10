43007e97cfae164d6a651bf197c6ed5eee9cd1d0
arturas <arturas19890228@gmail.com>
2024-01-10 23:50:28 +0200

    update nav buttons

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,5 +1,8 @@
 import React from "react"
 import Language from "../Language/Language"
+import { faTrash } from "@fortawesome/free-solid-svg-icons/faTrash"
+import { faList } from "@fortawesome/free-solid-svg-icons/faList"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 export default function NavBar(props) {
   const t = props.t
@@ -12,10 +15,10 @@ export default function NavBar(props) {
     <div className="navigation-bar">
       <div>
         <button className="button-navigate button-navigate__primary" onClick={() => handleDisplays("displayList")}>
-          {t("nav_bar.list")}
+          <FontAwesomeIcon icon={faList}/>
         </button>
         <button className="button-navigate button-navigate__primary" onClick={() => handleDisplays("displayDeletedItems")}>
-          {t("nav_bar.deleted_items")}
+          <FontAwesomeIcon icon={faTrash}/>
         </button>
       </div>
       <Language />
